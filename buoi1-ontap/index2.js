2dd3c4ef5e39f1c6f1449cd82d7bbf295ea2a6c4
HuongNTN <huongntn@payme.vn>
2022-07-31 16:20:54 +0700

    arrow func

---
+++ buoi1-ontap/index2.js
@@ -0,0 +1,26 @@
+// function getData(name) {
+//   return { name } // {name: name}
+// }
+
+
+// const data = getData();
+// console.log(data);
+
+
+// const getData = (name) => {
+//   return { name }
+// }
+
+// const getData = (name) => { return { name } };
+// const getData = (name) => 'Hello, ' + name;
+// const getData = (name) => `Hello, ${name}`;
+
+// const data = getData('BEN-5');
+// console.log(data);
+
+const arr = [1, 2, 3, 5, 7, 13, 90, 6, 8];
+// arr.forEach(function (item) {
+//   console.log(item);
+// });
+
+arr.forEach((item, i) => console.log(i));
