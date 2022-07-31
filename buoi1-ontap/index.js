08b0488f1ba3202e7557ee7d9eb8e7e1e8a471ea
HuongNTN <huongntn@payme.vn>
2022-07-31 15:42:42 +0700

    falsy

--- buoi1-ontap/index.js
+++ buoi1-ontap/index.js
@@ -1,2 +1,28 @@
-let a = 12;
-const b = "STATUS";
+// let a = -12;
+// const b = "STATUS";
+// const c = true;
+
+
+// //string number bool obj function undefined NaN
+
+// const x = () => { };
+// const y = [1, 4, 5];
+// const t = undefined;
+const z = undefined;
+const obj = {};
+let a = undefined + 1;
+// console.log(typeof a);
+a = '';
+a = [];
+a = {};
+
+// console.log(typeof z);
+// null 0 false undefined NaN ''
+
+if (!a) {
+  console.log("dieu kien dung");
+} else {
+  console.log("dieu kien sai");
+}
+// let name = undefined;
+// console.log(name);
