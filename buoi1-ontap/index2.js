234c8d76e1eb29e3f70e64abcc478d53e630055f
HuongNTN <huongntn@payme.vn>
2022-07-31 16:59:29 +0700

    function

--- buoi1-ontap/index2.js
+++ buoi1-ontap/index2.js
@@ -18,9 +18,47 @@
 // const data = getData('BEN-5');
 // console.log(data);
 
-const arr = [1, 2, 3, 5, 7, 13, 90, 6, 8];
+const arr = [1, 2, 3, 5, 7, 13, 90, 6, 8, 9, 17];
 // arr.forEach(function (item) {
 //   console.log(item);
 // });
 
-arr.forEach((item, i) => console.log(i));
+// arr.forEach((item, i) => console.log(i));
+// const a = 9;
+// console.log(Math.sqrt(a));
+// function printItem(item) {
+//   console.log(item);
+// }
+// function calc() {
+//   arr.forEach(printItem);
+// }
+
+// calc();
+
+// const soChinhPhuong = (num) => {
+//   const sqrt = parseInt(Math.sqrt(num));
+//   if (sqrt * sqrt === num) return true;
+//   return false;
+// }
+
+const soChinhPhuong = (num) => {
+  const sqrt = Math.sqrt(num); // 1.355666.
+  if (sqrt === parseInt(sqrt)) return true;
+  return false;
+}
+// console.log(soChinhPhuong(80));
+let result = 'Day so chinh phuong la: ';
+// arr.forEach(function (item) {
+//   if (soChinhPhuong(item)) {
+//     result += item + ' '; // result = result + item;
+//   }
+// });
+// arr.forEach(item => {
+//   if (soChinhPhuong(item)) {
+//     result += item + ' '; // result = result + item;
+//   }
+// });
+// console.log(result);
+
+// let n = 100; in ra day so chinh phuong tư 1->n
+// let n = 100; in ra day so nguyên tố tư 1->n
