c05e90acff7d80841d6eb62dcc44d054fc27e05d
HuongNTN <huongntn@payme.vn>
2022-08-06 20:26:34 +0700

    add bt

---
+++ buoi2/index2.js
@@ -0,0 +1,9 @@
+/**
+cho n = 1 -> 100;
+Viết 1 function để thực hiện các việc sau:
+- In dãy số chẳn
+- In dãy số lẻ
+- In dãy số chia 6 dư 5: 5,11,17,23,29
+- In dãy số chia 6 dư 4: 4,10,16,22,....
+- In dãy số chinh phuong
+*/
