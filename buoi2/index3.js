4bb03506ba74f3fd2d08543b29c04051d5ab28f5
HuongNTN <huongntn@payme.vn>
2022-08-07 14:25:59 +0700

    hinh3

--- buoi2/index3.js
+++ buoi2/index3.js
@@ -7,12 +7,17 @@ o--------->
 |
 |
 
- *                y=1 x=1
- * *              y=2 x=1,2
- * * *            y=3 x=1,2,3
- * * * *          y=4 x=1,2,3,4
+ *                x=1 y=1
+ * *              x=2 y=1,2
+ * * *            x=3 y=1,2,3
+ * * * *          x=4 y=1,2,3,4
  * * * * *
 
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
 */
 const n = 5;
 const hinh1 = (n) => {
@@ -37,28 +42,34 @@ const hinh2 = (n) => {
     console.log(starString);
   }
 }
-hinh1(n);
-console.log('\n');
-hinh2(n);
-
-
-
-
-
+// hinh1(n);
+// console.log('\n');
+// hinh2(n);
 
 /*
- * * * * *
- * * * *
- * * *
- * *
- *
 
- *
- * *
- *   *
- *     *
- * * * * *
+ *          x=1 y=1
+ * *        x=2 y=1,2
+ *   *      x=3 y=1,3
+ *     *    x=4 y=1,4
+ * * * * *  x=5 y=1,2,3,4,5 (ko phu thuoc y)
+*/
+const hinh3 = (n) => {
+  for (let x = 1; x <= 5; x++) {
+    let starString = '';
+    for (let y = 1; y <= 5; y++) {
+      if (y == 1 || x == y || x == 5) {
+        starString += '* ';
+      } else {
+        starString += '  ';
+      }
+    }
+    console.log(starString);
+  }
+}
+hinh3(n);
 
+/*
  * * * * *
  *     *
  *   *
