17c85be81b88b80d3bd419275be5e571f2c5bc4a
HuongNTN <huongntn@payme.vn>
2022-08-07 16:08:19 +0700

    class

---
+++ buoi3/index.js
@@ -0,0 +1,44 @@
+// const fn = (a, b) => {
+//   console.log(a, b);
+// }
+
+// const fn = (a, b, ...rest) => {
+//   console.log(a, b)
+//   console.log(rest);
+//   // console.log(arguments);
+// };
+// fn(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// const num = arr.splice(1, 1)[0];
+// console.log(num);
+// const [a, b, ...c] = arr;
+// console.log(a, b, c);
+// arr.push(9);
+// arr['name'] = 'Ben5';
+// arr.id = 10;
+// console.log(arr);
+
+// const obj = {
+//   id: 1,
+//   name: 'BEN5'
+// };
+// // const id = obj.id;
+// const { id } = obj;
+// console.log(id);
+
+// const arr = [
+//   {
+//     id: 1,
+//     name: 'Ben5',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ben6',
+//   }
+// ];
+// // console.log(arr[1].name);
+// const [, { id, name, age = 20 }] = arr;
+// console.log(age);
+// console.log(arr);
---
+++ buoi3/index2.js
@@ -0,0 +1,64 @@
+class Ben5 {
+  #id;
+  constructor(maSV = '-', tenSV = 'no name') {
+    this.#id = maSV;
+    this.name = tenSV;
+  }
+  getId() { return this.#id; }
+  getName = () => this.name;
+  // getter
+  get hello() {
+    return `Hello, ${this.name}`;
+  }
+
+  // setter
+  set diaChi(add) { // fn
+    ///
+    return this.address = add;
+  }
+}
+const b5 = new Ben5('1', 'NVA');
+// console.log(b5.hello);
+b5.diaChi = 'Quan 3'; // prop  diaChi
+console.log(b5.address);
+
+
+// console.log(b5.name);
+// b5.name = 'Nguyen Van B';
+// b5.age = 20;
+// console.log(b5);
+
+// console.log(b5.name);
+// console.log(b5.getId());
+// console.log(b5.getName());
+
+// const Ben5 = {
+//   id: 1,
+//   name: 'Ben5',
+//   getName: () => {
+//     console.log(this);
+//     return Ben5.name
+//   }
+// };
+// Ben5.getId = () => Ben5.id;
+
+// console.log(Ben5.getId());
+
+// console.log(Ben5.getName());
+/**
+Bai 1:
+Cho O(0,0) và A(5,6)
+Tính khoảng cách O->A;
+
+Bai 2;
+Cho A(5,6) B(1,1) C(8,16)
+Tính chu vi tam giác ABC
+ */
+
+class Point {
+  constructor(x, y) {
+  }
+}
+
+const A = new Point(5, 6);
+const O = new Point(0, 0);
