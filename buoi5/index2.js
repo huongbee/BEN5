a59f7764581792e955a5a1da57752d96d4aea343
HuongNTN <huongntn@payme.vn>
2022-08-14 16:20:10 +0700

    index3

--- buoi5/index2.js
+++ buoi5/index2.js
@@ -1,16 +1,16 @@
 const request = require('request');
 
 const location = 'SaiGon';
-const url = 'https://api.weatherapi.com/v1/current.json?key=b72b885b72f84ed681c42155220102&q=' + location;
-
-request.get(url, (error, response, body) => {
-  if (error) throw error;
-  const data = JSON.parse(body);
-  console.log(data.current.temp_c); // JSON.stringify(json)
-});
-
 const getTempC = (location) => {
-  return 25;
+  const url = 'https://api.weatherapi.com/v1/current.json?key=b72b885b72f84ed681c42155220102&q=' + location;
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) return reject(error);
+      const data = JSON.parse(body);
+      return resolve(data.current.temp_c); // JSON.stringify(json)
+    });
+  })
 }
-const tempc = getTempC(location);
-console.log(`Nhiet do hien tai o ${location} la: ${tempc}`);
+getTempC(location)
+  .then(result => console.log(`Nhiet do hien tai o ${location} la: ${result}`))
+  .catch(err => 'Lỗi: ' + console.log(err.message));
---
+++ buoi5/index3.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const cal = (a, b, pt, cb) => {
+  const url = `http://localhost:3000/${pt}/${a}/${b}`;
+  request.get(url, (error, response, body) => {
+    if (error) return cb(error, null);
+    const data = JSON.parse(body);
+    return cb(null, data.result);
+  });
+}
+// (3 + 4) * 5 / 2
+cal(3, 4, 'cong', (error, result) => {
+  if (error == null) {
+    cal(result, 5, 'nhan', (err, tich) => {
+      if (err == null) console.log(tich);
+      cal(tich, 2, 'chia', (err, thuong) => {
+        if (err == null) console.log(thuong);
+      })
+    })
+  }
+});
