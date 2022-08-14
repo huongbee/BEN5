feacdcaa0370b0024fff4e6394577eff1b1e01e0
HuongNTN <huongntn@payme.vn>
2022-08-14 15:20:56 +0700

    add location

--- buoi5/index2.js
+++ buoi5/index2.js
@@ -1,1 +1,16 @@
-// https://api.weatherapi.com/v1/current.json?key=b72b885b72f84ed681c42155220102&q=SaiGon
+const request = require('request');
+
+const location = 'SaiGon';
+const url = 'https://api.weatherapi.com/v1/current.json?key=b72b885b72f84ed681c42155220102&q=' + location;
+
+request.get(url, (error, response, body) => {
+  if (error) throw error;
+  const data = JSON.parse(body);
+  console.log(data.current.temp_c); // JSON.stringify(json)
+});
+
+const getTempC = (location) => {
+  return 25;
+}
+const tempc = getTempC(location);
+console.log(`Nhiet do hien tai o ${location} la: ${tempc}`);
