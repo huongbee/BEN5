3dd41cf5275bf60ef90196f5fbf7fc75c5f91749
HuongNTN <huongntn@payme.vn>
2022-08-27 19:29:47 +0700

    like

--- buoi7/index3.js
+++ buoi7/index3.js
@@ -30,12 +30,64 @@ const UserModel = mongoose.model('User', UserSchema); // users
 // })
 //   .then(result => console.log(result))
 //   .catch(err => console.log(err.message))
-UserModel.findOne({ id: 2 }) // select fullname from users where
-  .then(result => console.log(result.fullname))
-  .catch(err => console.log(err.message))
+// UserModel.findOne({
+//   id: 10,
+//   email: 'nguyenvana@gmail.com'
+// })
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err.message))
 // insert 50-100 users
 // select * from users where id=10
 // select fullname from users where id=10 and email=nguyenvana@gmail.com
+// select fullname from users where id=10 or email=nguyenvana@gmail.com
 // select * from users where name like 'Nguyen%'
 // select * from users limit 0,10
 //
+// for (let i = 3; i <= 100; i++) {
+//   UserModel.create({
+//     id: i,
+//     fullname: 'Nguyen Van B ' + i,
+//     email: `nguyenvanb_${i}@gmail.com`
+//   })
+// };
+// UserModel.find({
+//   $or: [ // $or | $and | $gte | $lte | $lt | $gt | $exist | $eq
+//     { id: 10 },
+//     { email: 'nguyenvana@gmail.com' }
+//   ]
+// })
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err.message))
+
+// cach 1
+// UserModel.findOne({
+//   id: 10,
+//   email: 'nguyenvana@gmail.com'
+// })
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err.message))
+// cach 2
+// UserModel.find({
+//   $and: [
+//     { id: { $eq: 10 } },
+//     { email: 'nguyenvana@gmail.com' }
+//   ]
+// })
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err.message))
+
+// UserModel.find({
+//   id: { $lt: 20 }  // id<10
+// }).sort({
+//   // id: 'asc'
+//   id: 1
+// }).skip(10).limit(10) // = limit 10,10
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err.message))
+
+
+UserModel.find({
+  fullname: { $regex: /([a-zA-Z ])Van([a-zA-Z ])/ } // like '%Van%'
+})
+  .then(result => console.log(result))
+  .catch(err => console.log(err.message))
