843471f8a1a2090d7ac9c7950ac058ba731b8dbb
HuongNTN <huongntn@payme.vn>
2022-09-10 20:57:31 +0700

    done bt1

--- buoi9/baitap1.js
+++ buoi9/baitap1.js
@@ -1,3 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const users = require('./data/users.json');
+const fs = require('fs');
+const app = express();
+app.use(bodyParser.json());
+
+
+app.post('/users', (request, response) => {
+  response.json({
+    success: true,
+    message: 'Success!',
+    data: { users }
+  });
+})
+
+app.post('/add', (request, response) => {
+  const user = request.body;
+  users.push(user);
+  fs.writeFileSync('./data/users.json', JSON.stringify(users));
+  response.json({
+    success: true,
+    message: 'Success!'
+  });
+})
+
+app.post('/update/:id', (request, response) => {
+  const id = +request.params.id;
+  const index = users.findIndex(user => user.id === id);
+  if (index < 0) {
+    return response.json({
+      success: false,
+      message: 'Can not find user id ' + id
+    });
+  }
+  users[index] = {
+    id: users[index].id, // not update
+    name: request.body.name || users[index].name,
+    age: request.body.age || users[index].age,
+    address: request.body.address || users[index].address
+  };
+  fs.writeFileSync('./data/users.json', JSON.stringify(users));
+  response.json({
+    success: true,
+    message: 'Success!'
+  });
+})
+
+app.post('/delete/:id', (request, response) => {
+  const id = +request.params.id;
+  const index = users.findIndex(user => user.id === id);
+  if (index < 0) {
+    return response.json({
+      success: false,
+      message: 'Can not find user id ' + id
+    });
+  }
+  users.splice(index, 1);
+  fs.writeFileSync('./data/users.json', JSON.stringify(users));
+  response.json({
+    success: true,
+    message: 'Success!'
+  });
+})
+app.listen(3000, () => console.log('Server listening on port 3000'));
+
+
+
+
+
+
+
+
+
+
+
 /*
 Cho file users.json
 Dựng server thực hiện các APIs sau:
